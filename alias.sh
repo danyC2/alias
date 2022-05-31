9dffced578d40cdb529ca6fce1451099f116465b
Daniel <user@mail.com>
2022-05-31 22:48:07 +0200

    update

--- alias.sh
+++ alias.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-# how to launch: . ./alias.sh
+# launching: . ./alias.sh
 
 alias gad='git add -A'
 alias gcm='git commit -m '
 alias gst='git status'
 alias gtr='git log --graph --oneline --decorate --all'
+
+alias c='clear'
+alias j='jobs -l'
+alias l='ls -AlFh --time-style=long-iso --color=auto'
+alias h='history'
+
+alias cp='cp -i'
+alias ln='ln -i'
+alias mkdir='mkdir -pv'
+alias mv='mv -iv'
+
+alias dir10='du -cksh * | sort -hr | head -n 10'
